bfab4a6576382bdba5e6aad065ad5a00950ab372
pixhawk <mavteam@student.ethz.ch>
2010-12-07 18:24:53 +0100

    Debugged SegFault problem in lcmtoros when closing the program with CTRL-C

--- lcm_mavlink_ros/src/lcmtoros.cpp
+++ lcm_mavlink_ros/src/lcmtoros.cpp
@@ -8,11 +8,17 @@
 
 ros::Publisher mavlink_pub;
 
+std::string lcmurl = "udpm://"; ///< host name for UDP server
+bool verbose;
+
+lcm_t *lcm;
+
 static void
 mavlink_handler (const lcm_recv_buf_t *rbuf, const char * channel,
 		const mavlink_message_t* msg, void * user)
 {
-	printf("Received message #%d on channel \"%s\" (sys:%d|comp:%d):\n", msg->msgid, channel, msg->sysid, msg->compid);
+	if (verbose)
+		ROS_INFO("Received message #%d on channel \"%s\" (sys:%d|comp:%d):\n", msg->msgid, channel, msg->sysid, msg->compid);
 
 	/**
 	 * Serialize the Mavlink-ROS-message
@@ -37,6 +43,7 @@ void* lcm_wait(void* lcm_ptr) {
 	lcm_t* lcm = (lcm_t*) lcm_ptr;
 	// Blocking wait for new data
 	while (1) {
+		if (verbose) printf("LCM Handle...");
 		lcm_handle(lcm);
 	}
 	return NULL;
@@ -45,6 +52,26 @@ void* lcm_wait(void* lcm_ptr) {
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "lcmtoros");
 
+	// Handling Program options
+	static GOptionEntry entries[] =
+	{
+			{ "lcmurl", 'l', 0, G_OPTION_ARG_STRING, &lcmurl, "LCM Url to connect to", "udpm://" },
+			{ "verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose, "Be verbose", NULL },
+			{ NULL }
+	};
+
+	GError *error = NULL;
+	GOptionContext *context;
+
+	context = g_option_context_new ("- translate MAVLink messages from LCM to ROS (Topic: mavlink)");
+	g_option_context_add_main_entries (context, entries, NULL);
+	//g_option_context_add_group (context, NULL);
+	if (!g_option_context_parse (context, &argc, &argv, &error))
+	{
+		g_print ("Option parsing failed: %s\n", error->message);
+		exit (1);
+	}
+
 	ros::NodeHandle n;
 
 	mavlink_pub = n.advertise<lcm_mavlink_ros::Mavlink> ("mavlink", 1000);
@@ -52,7 +79,7 @@ int main(int argc, char **argv) {
 	/**
 	 * Connect to LCM Channel and register for MAVLink messages
 	 */
-	lcm_t *lcm = lcm_create("udpm://");
+	lcm_t* lcm = lcm_create(lcmurl.c_str());
 	if (!lcm) {
 		return 1;
 	}
@@ -60,6 +87,10 @@ int main(int argc, char **argv) {
 	mavlink_message_t_subscription_t * comm_sub = mavlink_message_t_subscribe(
 			lcm, "MAVLINK", &mavlink_handler, NULL);
 
+	while (ros::ok())
+		lcm_handle(lcm);
+
+	/*
 	// Initialize LCM receiver thread
 	GThread* lcm_thread;
 	GError* err;
@@ -73,20 +104,23 @@ int main(int argc, char **argv) {
 			TRUE, &err)) == NULL) {
 		printf("Thread create failed: %s!!\n", err->message);
 		g_error_free ( err);
-	}
+	}*/
 
-	ros::Rate loop_rate(1);
+	//ros::Rate loop_rate(1);
 
 	/**
 	 * Now just wait until the process is terminated...
 	 */
-	while (ros::ok()) {
-		loop_rate.sleep();
-	}
+	//while (ros::ok()) {
+	//	loop_rate.sleep();
+	//}
 
+	if (verbose) printf("Trying MAVLink unsubscribe");
 	mavlink_message_t_unsubscribe (lcm, comm_sub);
+	if (verbose) printf("Trying LCM destroy");
 	lcm_destroy (lcm);
-	g_thread_join(lcm_thread);
+//	if (verbose) printf("Trying GThread Join");
+//	g_thread_join(lcm_thread);
 
 	return 0;
 }
--- lcm_mavlink_ros/src/rostolcm.cpp
+++ lcm_mavlink_ros/src/rostolcm.cpp
@@ -5,6 +5,10 @@
 #include "mavlinkros.h"
 
 #include <sstream>
+#include <glib.h>
+
+std::string lcmurl = "udpm://"; ///< host name for UDP server
+bool verbose;
 
 /**
  * Grabs all mavlink-messages from the ROS-Topic "mavlink" and publishes them on the LCM-Mavlink-Channel
@@ -31,12 +35,32 @@ void mavlinkCallback(const lcm_mavlink_ros::Mavlink::ConstPtr& mavlink_ros_msg)
 	 */
 	mavlink_message_t_publish (lcm, "MAVLINKROS", &msg);
 
-	//DEBUG
-	ROS_INFO("Sent Mavlink from ROS to LCM, Message-ID: [%i]", mavlink_ros_msg->msgid);
+	if (verbose)
+		ROS_INFO("Sent Mavlink from ROS to LCM, Message-ID: [%i]", mavlink_ros_msg->msgid);
 }
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "rostolcm");
+
+	// Handling Program options
+	static GOptionEntry entries[] =
+	{
+			{ "lcmurl", 'l', 0, G_OPTION_ARG_STRING, &lcmurl, "LCM Url to connect to", "udpm://" },
+			{ "verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose, "Be verbose", NULL },
+			{ NULL }
+	};
+
+	GError *error = NULL;
+	GOptionContext *context;
+
+	context = g_option_context_new ("- translate MAVLink messages from ROS to LCM");
+	g_option_context_add_main_entries (context, entries, NULL);
+	//g_option_context_add_group (context, NULL);
+	if (!g_option_context_parse (context, &argc, &argv, &error))
+	{
+		g_print ("Option parsing failed: %s\n", error->message);
+		exit (1);
+	}
 
 	ros::NodeHandle n;
 
@@ -45,7 +69,7 @@ int main(int argc, char **argv) {
 	/**
 	 * Connect to LCM Channel and register for MAVLink messages ->
 	 */
-	lcm = lcm_create("udpm://");
+	lcm = lcm_create(lcmurl.c_str());
 	if (!lcm) {
 		return 1;
 	}
